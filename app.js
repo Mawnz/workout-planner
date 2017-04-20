8004b4dd61dc10c4ff9b532c76d719173ed24a68
Mawnz <mans.aberg@gmail.com>
2017-04-20 13:15:08 +0200

    rip

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 app.use(epxress.static(__dirname + '/app'));
 
-app.listen(app.get('port'), function(){
-	console.log('Listening on port ', app.get('port'));
-});
+app.listen(process.env.PORT || 3000);
