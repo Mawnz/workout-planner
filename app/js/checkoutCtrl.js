1b6d45fbeef4ec54d0c6c36c876469175127d812
Mawnz <mans.aberg@gmail.com>
2017-04-20 11:29:51 +0200

    THIS IS BEYOND PROJECT DEADLINE DON'T MIND
    Added cross on dialog pop-up to close. Some tweaks in package.json

--- app/js/checkoutCtrl.js
+++ app/js/checkoutCtrl.js
@@ -1,32 +1,5 @@
 projectTrainingApp.controller('CheckoutCtrl', function ($scope,Workout, $sce) {
 	$scope.eMenu = Workout.getMyWorkout();
 
-	$scope.download = function(){
-		var printDoc = new jsPDF();
-    	printDoc.fromHTML($('#printMe').get(0), 10, 10, {'width': 180});
-   		printDoc.autoPrint();
-   		printDoc.output("dataurlnewwindow");
-		/*
-		var doc = new jsPDF();
-		var elementHandler = {
-			'#editor': function(element, renderer){
-				return true;
-			}
-		};
-		var source = $("#printMe")[0];
-
-		doc.fromHTML(
-			source,
-			15, 
-			15, 
-			{
-				width: 180, 
-				"elementHandlers": elementHandler
-			});
-
-		doc.output("dataurlnewwindow");	
-		*/
-	}
-
 });
 
